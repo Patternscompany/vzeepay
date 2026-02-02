b5f46eb65054025fd4285029ad997643da53698a
Patternscompany <pandureddypatterns@gmail.com>
2026-02-02 18:27:11 +0530

    pp

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ mode }) => ({
-  base: '/', // ✅ ALWAYS '/' for Netlify
+export default defineConfig({
+  base: '/', // ✅ Netlify
   plugins: [
     react(),
     tailwindcss(),
     tsconfigPaths(),
   ],
-  build: {
-    outDir: 'dist',
-  },
-}));
+});
+
 
